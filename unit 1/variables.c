e6594a53f5b331fdf986a4e7b845583b68e2cc34
RudyChavez <rudyivan.chavezquintal69@gmail.com>
2022-02-16 11:41:26 -0600

    added variables.c

--- unit 1/variables.c
+++ unit 1/variables.c
@@ -1,8 +1,35 @@
 #include <stdio.h> //its just a comment     
 
 //Datatype:
+//create a variable: dataType anyName; | DataType anyName = 12; // solo se declara una vez
+
+int myInt = 24; //-> text format: %i %d 
+float myFloat = 22.7; //-> text format: %f
+char myChar = 'l';
+int myArray[7] = { 5, 27, 43, 12, 8, 7, 0 };
+                // 0, 1, 2, 3, 4, 5, 6 ...n 
+char word[4] = {'R','u','d','y'};
+char country[10] = "mexico"; // %s
+char* aName = "Ivan"; 
 
 int main(){
 
+    myInt = 25; 
+    printf("hola clase, myInt = %i\n", myInt);
+   printf("hola clase, myFloat = %f\n", myFloat);
+   printf("hola clase, myChar = %c\n", myChar);
+   printf("hola clase, myArray1 = %d\n", myArray[0]);
+    printf("hola clase, myArray2 = %d\n", myArray[1]);
+    printf("hola clase, myArray3 = %d\n", myArray[2]);
+    printf("hola clase, myArray4 = %d\n", myArray[3]);
+    printf("hola clase, myArray5 = %d\n", myArray[4]);
+    printf("hola clase, myArray6 = %d\n", myArray[5]);
+    printf("hola clase, myArray7 = %d\n", myArray[6]);
+    printf("hola clase, word = %c\n", word[0]);
+    printf("hola clase, word = %c\n", word[1]);
+    printf("hola clase, word = %c\n", word[2]);
+    printf("hola clase, word = %c\n", word[3]);
+    printf("hola clase, country = %s\n", country);
+    printf("hola clase, aName = %s\n", aName);
     return 0;
 }
