98267e4425b1c7500563ac5ef19130715d1d2a2b
RudyChavez <rudyivan.chavezquintal69@gmail.com>
2022-03-10 08:10:09 -0600

    rearrangement of folders

---
+++ unit 2/loops.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(int argc, char** argv){
+
+    for(int numero = 1 ; numero <= 5 ; numero = numero + 1)(
+        printf("el numero actual es : %d\n", numero)
+
+    );
+
+
+
+}
